9913ea53f48bc3a5c1c2d25d0ac069ce5269feaa
Anees Patwa <35473847+aneespatwa@users.noreply.github.com>
2020-10-04 17:14:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cse247-m1lab1
+Baseline student repo for CSE247/502N M1:Lab.
---
+++ labs/simple/Simple.java
@@ -0,0 +1,33 @@
+package simple;
+
+public class Simple {
+
+	private int data;
+
+	/**
+	 * Initialize data to -1.
+	 */
+	public Simple() {
+        data = -1;
+	}
+	
+	/**
+     * Get value of data.
+     * @return data
+	 */
+	public int getData() {
+        // FIXME: replace the following statement to return the
+        // appropriate value.
+        return 0;
+    }
+
+    /**
+     * Set value of data.
+     * @param int value to give to instance variable
+     */
+    public void setData(int value) {
+        // FIXME: replace the following statement to set 
+        // data equal to value.
+        data = 0;
+    }
+}
